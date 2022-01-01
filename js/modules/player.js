3529610073c03c36b6d261bad3a60ee4444edc50
Maksim Zakharenkov <maxxzahar@rambler.ru>
2022-01-01 19:48:42 +0300

    just a checkpoint, basic styling is in process

--- js/modules/player.js
+++ js/modules/player.js
@@ -5,5 +5,11 @@ export class Player {
         this.cards = cards;
         this.points = 0;
         this.effects = [];
+        this.owned = [];
+    }
+    display() {
+        const container = document.querySelector('.wrapper');
+        container.insertAdjacentHTML('beforeend', `<div class="player"><h2 class="player__name">${this.name}</h2><div class="player__points-counter">${this.points}</div><div class="player__stages-counter"></div></div>`);
+        container.querySelector('.player').style.backgroundColor = this.color;
     }
 }
--- js/script.js
+++ js/script.js
@@ -2,3 +2,4 @@ import { settings } from "./modules/settings.js";
 const { players, deck } = settings;
 console.log(players);
 console.log(deck);
+players[0].display();
